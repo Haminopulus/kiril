1b8bf1958ebdc357f82762e7cde27e932bf03d0b
Haminopulus <haminopulus@gmail.com>
2025-10-16 02:10:29 +0200

    feat: now using Path instead of str

--- src/lrc.rs
+++ src/lrc.rs
@@ -1,20 +1,31 @@
+use async_std::path::Path;
 use mpris::Metadata;
 use urlencoding::decode;
 use regex::Regex;
+use std::fs::read_dir;
 
+//const PATH: Regex = Regex::new(r"/[^/]+/.*").unwrap();
+//const FILE_NAME: Regex = Regex::new(r"([^/]*)\..*$").unwrap();
 
 fn get_lyric_file(metadata: Metadata) {
-    let path: &str = metadata.url().unwrap();
-    // TODO: these do not need to be compiled every time, store them somewhere
-    // FUNFACT: pee is stored in the balls
-    let file_path = Regex::new(r"/[^/]+/.*").unwrap();
-    let file_name = Regex::new(r"([^/]*$)").unwrap();
-    // for now only local files are supported
-    let result = file_path.find(path);
-    if path.starts_with("file://") {
-        let file_name: &str = path;  
+    let url: &str = decode(metadata
+        .url()
+        .unwrap_or_default());
+
+    if url.starts_with("file://") {
+        let path = Path::new(url.trim_start_matches("file://"));
+        let file_name: &str = path
+            .file_stem()
+            .unwrap_or_default();
+        if !file_name.is_empty() {
+            // find lrc file in parent-, current or subdirectories
+            todo!();
+            //read_dir(path)
+            //    .unwrap().find(|x| x.unwrap().path().file_name() == file_name+".lrc");
+        }
     }
 }
+
 
 pub fn get_current_lyrics() {
 
