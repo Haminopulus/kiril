34c54f360878971b707cf38ab03b95c8d211b0ff
Haminopulus <haminopulus@gmail.com>
2025-10-17 19:39:17 +0200

    more

--- src/lrc.rs
+++ src/lrc.rs
@@ -1,30 +1,67 @@
-use async_std::path::Path;
+use std::{fs::read_dir, path::Path, io::Result};
 use mpris::Metadata;
 use urlencoding::decode;
-use regex::Regex;
-use std::fs::read_dir;
 
-//const PATH: Regex = Regex::new(r"/[^/]+/.*").unwrap();
-//const FILE_NAME: Regex = Regex::new(r"([^/]*)\..*$").unwrap();
 
-fn get_lyric_file(metadata: Metadata) {
-    let url: &str = decode(metadata
-        .url()
-        .unwrap_or_default());
-
+fn get_lyric_file(metadata: Metadata) -> Option<String> {
+    let url: String = metadata.url()?.into();
     if url.starts_with("file://") {
+        let url: String = match decode(&url) {
+            Ok(url) => url.into_owned(),
+            _ => { return None; }
+        };
         let path = Path::new(url.trim_start_matches("file://"));
-        let file_name: &str = path
-            .file_stem()
-            .unwrap_or_default();
-        if !file_name.is_empty() {
-            // find lrc file in parent-, current or subdirectories
-            todo!();
-            //read_dir(path)
-            //    .unwrap().find(|x| x.unwrap().path().file_name() == file_name+".lrc");
+        let file_name: String = format!("{}.lrc", path.file_stem()?.to_str()?);
+        assert!(path.is_absolute());
+        search_dir(file_name, path.parent().unwrap());
+    }
+    return Some("".to_owned());
+}
+
+//
+//    def _find_lrc(self, path: str, filename: str) -> str:
+//        dirs, files = self._listdir_abs(path)
+//        pdirs, pfiles = self._listdir_abs(path + "..")
+//        for file in files + pfiles:
+//            if os.path.basename(str(file)) == self._to_lrc(filename):
+//                return str(file)
+//        for dir_ in dirs + pdirs:
+//            _, dirfiles = self._listdir_abs(dir_)
+//            for file in dirfiles:
+//                if os.path.basename(str(file)) == self._to_lrc(filename):
+//                    return str(file)
+//        return ""
+//
+
+/// non-recursively search a directory for a file with given file_name
+fn search_dir(file_name: String, dir: &Path, depth: u16) -> Result<String, String> {
+    if dir.is_dir() {
+        for entry in read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() && depth > 0 {
+                search_dir(file_name, &path, depth-1)?;
+            } else {
+                let file: String = path.file_name()
+                    .unwrap()
+                    .to_os_string()
+                    .into_string()
+                    .unwrap();
+                if file == file_name {
+                    let found: String = path.to_str().unwrap_or_default().into();
+                    Ok(found);
+
+                }
+            }
         }
     }
+    Err("".into());
 }
+
+    //let entries = read_dir(path)
+    //    .map(|x| x.unwrap().path())
+    //    .find(|x| x.file_stem().unwrap_or_default() == file_name)
+    //    .to_str().unwrap().to_owned()
 
 
 pub fn get_current_lyrics() {
