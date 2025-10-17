86a64a07faf4436ff4178f60ed82934c11237ff8
Haminopulus <haminopulus@gmail.com>
2025-10-18 00:07:33 +0200

    feat: lrc file search function, needs to be tested now

--- src/lrc.rs
+++ src/lrc.rs
@@ -2,7 +2,7 @@ use std::{fs::read_dir, path::Path, io::Result};
 use mpris::Metadata;
 use urlencoding::decode;
 
-
+/// based on currently playing file, find corresponding .lrc-file if it exists near the song file
 fn get_lyric_file(metadata: Metadata) -> Option<String> {
     let url: String = metadata.url()?.into();
     if url.starts_with("file://") {
@@ -13,34 +13,30 @@ fn get_lyric_file(metadata: Metadata) -> Option<String> {
         let path = Path::new(url.trim_start_matches("file://"));
         let file_name: String = format!("{}.lrc", path.file_stem()?.to_str()?);
         assert!(path.is_absolute());
-        search_dir(file_name, path.parent().unwrap());
+        match search_dir(file_name, path.parent()?, 1) {
+            Ok(path) => return Path::new(&path),
+            Err(_) => {
+                match search_dir(file_name, path.parent()?.parent()?, 1) {
+                    Ok(path) => return Path::new(&path),
+                    Err(_) => return None
+                }
+            }
+        }; // Search for the song file, this could probably look a lot better
     }
-    return Some("".to_owned());
+    return None
 }
 
-//
-//    def _find_lrc(self, path: str, filename: str) -> str:
-//        dirs, files = self._listdir_abs(path)
-//        pdirs, pfiles = self._listdir_abs(path + "..")
-//        for file in files + pfiles:
-//            if os.path.basename(str(file)) == self._to_lrc(filename):
-//                return str(file)
-//        for dir_ in dirs + pdirs:
-//            _, dirfiles = self._listdir_abs(dir_)
-//            for file in dirfiles:
-//                if os.path.basename(str(file)) == self._to_lrc(filename):
-//                    return str(file)
-//        return ""
-//
-
-/// non-recursively search a directory for a file with given file_name
+/// search the directory for given file_name with given recursive depth
 fn search_dir(file_name: String, dir: &Path, depth: u16) -> Result<String, String> {
     if dir.is_dir() {
         for entry in read_dir(dir)? {
             let entry = entry?;
             let path = entry.path();
             if path.is_dir() && depth > 0 {
-                search_dir(file_name, &path, depth-1)?;
+                match search_dir(file_name, &path, depth-1) {
+                    Ok(found) => Ok(found),
+                    Err(_) => continue
+                };
             } else {
                 let file: String = path.file_name()
                     .unwrap()
@@ -49,20 +45,14 @@ fn search_dir(file_name: String, dir: &Path, depth: u16) -> Result<String, Strin
                     .unwrap();
                 if file == file_name {
                     let found: String = path.to_str().unwrap_or_default().into();
-                    Ok(found);
+                    Ok(found)
 
                 }
             }
         }
     }
-    Err("".into());
+    Err("".into())
 }
-
-    //let entries = read_dir(path)
-    //    .map(|x| x.unwrap().path())
-    //    .find(|x| x.file_stem().unwrap_or_default() == file_name)
-    //    .to_str().unwrap().to_owned()
-
 
 pub fn get_current_lyrics() {
 
